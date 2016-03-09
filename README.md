fabb9b1f6b1fca64e89959e24f801c22326a38e0
Sanko Robinson <sanko@cpan.org>
2016-03-09 11:43:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Finance::Robinhood - It's new $module
+
+# SYNOPSIS
+
+    use Finance::Robinhood;
+
+# DESCRIPTION
+
+Finance::Robinhood is ...
+
+# LICENSE
+
+Copyright (C) Sanko Robinson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Sanko Robinson &lt;sanko@cpan.org>
---
+++ lib/Finance/Robinhood.pm
@@ -0,0 +1,39 @@
+package Finance::Robinhood;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Finance::Robinhood - It's new $module
+
+=head1 SYNOPSIS
+
+    use Finance::Robinhood;
+
+=head1 DESCRIPTION
+
+Finance::Robinhood is ...
+
+=head1 LICENSE
+
+Copyright (C) Sanko Robinson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Sanko Robinson E<lt>sanko@cpan.orgE<gt>
+
+=cut
+
