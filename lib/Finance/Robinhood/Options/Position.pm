9b5b88ff80d487d21e9d9ab1f129c283d9bd55f3
Sanko Robinson <sanko@cpan.org>
2018-05-06 08:30:45 -0400

    Break option out intn object wrapper

--- lib/Finance/Robinhood/Options/Position.pm
+++ lib/Finance/Robinhood/Options/Position.pm
@@ -5,7 +5,6 @@ use Date::Tiny;
 has [
     qw[account average_price chain_id chain_symbol id
         intraday_average_open_price intraday_quantity
-        option
         quantity pending_buy_quantity pending_expired_quantity pending_sell_quantity
         type url
         ]
@@ -18,6 +17,17 @@ has [ 'created_at', 'updated_at' ] => (
         # BUG: DateTime::Tiny cannot handle sub-second values.
         $_[0] =~ s'\..+$'';
         DateTime::Tiny->from_string( $_[0] );
+    }
+);
+has '_option_url' => ( is => 'ro', init_arg => 'option' );
+has 'option' => (
+    is       => 'ro',
+    lazy     => 1,
+    init_arg => undef,
+    builder  => sub {
+        my ( $status, $data )
+            = Finance::Robinhood::Utils::Client->instance->get( $_[0]->_option_url );
+        $status == 200 ? Finance::Robinhood::Options::Instrument->new($data) : $data;
     }
 );
 1;
