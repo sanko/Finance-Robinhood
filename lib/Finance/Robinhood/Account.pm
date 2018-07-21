edad51809d73c3280bac844ac8ad5b37908d351b
Sanko Robinson <sanko@cpan.org>
2018-07-21 00:36:26 -0400

    Get positions and check downgrade ability

--- lib/Finance/Robinhood/Account.pm
+++ lib/Finance/Robinhood/Account.pm
@@ -3,6 +3,7 @@ use Moo;
 with 'MooX::Singleton';
 use Time::Moment;
 #
+use Finance::Robinhood::Equity::Position;
 use Finance::Robinhood::Account::InstantEligibility;
 use Finance::Robinhood::Account::MarginBalances;
 use Finance::Robinhood::Account::Portfolio;
@@ -26,13 +27,13 @@ has [ 'created_at', 'updated_at' ] => (
         Time::Moment->from_string( $_[0] );
     }
 );
-has '_portfolio_url' => (
+has '_' . $_ . '_url' => (
     is       => 'ro',
-    init_arg => 'portfolio',
+    init_arg => $_,
     coerce   => sub {
         ref $_[0] ? $_[0]->url : $_[0];
     }
-);
+) for qw[portfolio positions can_downgrade_to_cash];
 has 'portfolio' => (
     is       => 'ro',
     lazy     => 1,
@@ -43,8 +44,25 @@ has 'portfolio' => (
         $status == 200 ? Finance::Robinhood::Account::Portfolio->new($data) : ();
     }
 );
-has [
-    qw[can_downgrade_to_cash positions
-        ]
-] => ( is => 'ro' );
+has 'positions' => (
+    is       => 'ro',
+    lazy     => 1,
+    init_arg => undef,
+    builder  => sub {
+        Finance::Robinhood::Utils::Paginated->new(
+            class => 'Finance::Robinhood::Equity::Position',
+            next  => shift->_positions_url
+        );
+    }
+);
+has 'can_downgrade_to_cash' => (
+    is       => 'ro',
+    lazy     => 1,
+    init_arg => undef,
+    builder  => sub {
+        my ( $status, $data )
+            = Finance::Robinhood::Utils::Client->instance->get( shift->_can_downgrade_to_cash_url );
+        $status == 200 ? $data->{can_downgrade_to_cash} : ()    # simple boolean
+    }
+);
 1;
